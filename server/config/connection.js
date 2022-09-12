0f8d60f236cbc8e9da13654dc1f80b64acf7c873
Vince <starryblue7@gmail.com>
2022-09-12 14:52:49 -0700

    set up db connection

---
+++ server/config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/react-stripe-store",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+module.exports = mongoose.connection;
