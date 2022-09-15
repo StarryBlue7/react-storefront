9b53e3dc3f55fb6c75cf72949df24379e5637093
Vince <starryblue7@gmail.com>
2022-09-15 14:59:48 -0700

    migrate category data to object tree

---
+++ server/data/category-tree.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    name: "Apparel",
+    subcategories: [
+      {
+        name: "Tops",
+        subcategories: [
+          { name: "T-Shirts" },
+          { name: "Jackets" },
+          { name: "Sweaters" },
+        ],
+      },
+      {
+        name: "Bottoms",
+        subcategories: [{ name: "Pants" }, { name: "Shorts" }],
+      },
+      {
+        name: "Hats",
+      },
+    ],
+  },
+  {
+    name: "Home & Garden",
+    subcategories: [
+      {
+        name: "Mugs",
+      },
+      {
+        name: "Decor",
+        subcategories: [{ name: "Art Prints" }, { name: "Figurines" }],
+      },
+    ],
+  },
+  {
+    name: "Toys & Games",
+    subcategories: [
+      {
+        name: "Plush Toys",
+      },
+    ],
+  },
+];
