34dc2edefa4a1aac27bf7013e4d2783bba209b31
Vince <starryblue7@gmail.com>
2022-09-15 12:15:13 -0700

    import/export order, category, & tag models

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,7 @@
-const User = require('./User');
-const Product = require('./Product');
+const User = require("./User");
+const Product = require("./Product");
+const Order = require("./Order");
+const Category = require("./Category");
+const Tag = require("./Tag");
 
-module.exports = { User, Product };
+module.exports = { User, Product, Order, Category, Tag };
