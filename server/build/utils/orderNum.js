dd1ab58f2f3655cd5f15547648fb6a9a9e85b2fb
Vince <starryblue7@gmail.com>
2022-09-21 15:17:19 -0700

    add order id generator

---
+++ server/build/utils/orderNum.js
@@ -0,0 +1,16 @@
+var uuid = require("uuid").v4;
+/**
+ * Generate a unique order id with prepended date & time
+ * @returns {String} Unique order id with prepended date & time
+ */
+function newOrderId() {
+    var now = new Date().toISOString();
+    // Prepend creation date as YYYY-MM-DD
+    var date = now.substring(0, 10);
+    // Time as HHMM
+    var time = now.substring(11, 13) + now.substring(14, 16);
+    // Append unique identifier
+    var uniqueId = uuid();
+    return "".concat(date, "-").concat(time, "-").concat(uniqueId);
+}
+module.exports = { newOrderId: newOrderId };
---
+++ server/utils/orderNum.ts
@@ -0,0 +1,19 @@
+const {v4: uuid} = require("uuid");
+
+/**
+ * Generate a unique order id with prepended date & time
+ * @returns {String} Unique order id with prepended date & time
+ */
+function newOrderId() {
+  const now = new Date().toISOString();
+  // Prepend creation date as YYYY-MM-DD
+  const date = now.substring(0, 10);
+  // Time as HHMM
+  const time = now.substring(11, 13) + now.substring(14, 16);
+  // Append unique identifier
+  const uniqueId = uuid();
+
+  return `${date}-${time}-${uniqueId}`;
+}
+
+module.exports = { newOrderId };
