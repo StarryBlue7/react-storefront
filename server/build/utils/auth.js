5ff8829f323f60baee6b550c8eab17d1a614f848
Vince <starryblue7@gmail.com>
2022-09-22 14:56:47 -0700

    add JWT auth util

---
+++ server/build/utils/auth.js
@@ -0,0 +1,28 @@
+var jwt = require("jsonwebtoken");
+var secret = process.env.JWT_SECRET;
+var expiration = "6h";
+module.exports = {
+    authMiddleware: function (_a) {
+        var req = _a.req;
+        var token = req.body.token || req.query.token || req.headers.authorization;
+        if (req.headers.authorization) {
+            token = token.split(" ").pop().trim();
+        }
+        if (!token) {
+            return req;
+        }
+        try {
+            var data = jwt.verify(token, secret, { maxAge: expiration }).data;
+            req.user = data;
+        }
+        catch (_b) {
+            console.log("Invalid token");
+        }
+        return req;
+    },
+    signToken: function (_a) {
+        var email = _a.email, username = _a.username, _id = _a._id;
+        var payload = { email: email, username: username, _id: _id };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
