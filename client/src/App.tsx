488f992bf931f2c043acfabd82a1e7192590ffdf
Vince <starryblue7@gmail.com>
2022-09-23 17:38:29 -0700

    working tags query & rendering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import logo from "./logo.svg";
+import TagList from "./components/tagList";
 import "./App.css";
 
 // Connection to GraphQL API
@@ -12,22 +12,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <TagList />
     </ApolloProvider>
   );
 }
