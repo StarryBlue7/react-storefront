3a3e56782c09542662aafa9661925a3def1426da
Vince <starryblue7@gmail.com>
2022-09-23 17:17:05 -0700

    setup Apollo provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,34 @@
 import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import logo from "./logo.svg";
 import "./App.css";
 
+// Connection to GraphQL API
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
