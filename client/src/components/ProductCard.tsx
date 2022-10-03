05690ef6dc3205081015e9e4d30ea0ad3851f8a3
Vince <starryblue7@gmail.com>
2022-10-03 10:54:10 -0700

    add tags to product cards

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+import { Button, CardActionArea, CardActions, Chip } from "@mui/material";
 
 export default function ProductCard({ product }: any) {
   return (
@@ -11,13 +11,32 @@ export default function ProductCard({ product }: any) {
       <CardActionArea>
         <CardMedia
           component="img"
-          height="300"
+          height="280"
           image={product.imgURL}
           alt={product.fullName}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {product.shortName}
+          </Typography>
+          <Typography
+            variant="body2"
+            textOverflow={"ellipsis"}
+            noWrap
+            color="text.secondary"
+            gutterBottom
+          >
+            {product.tags.map((tag: any, i: number) => (
+              <>
+                <Chip
+                  key={i}
+                  label={tag.name}
+                  variant="outlined"
+                  size="small"
+                  onClick={() => {}}
+                />{" "}
+              </>
+            ))}
           </Typography>
           <Typography
             variant="body2"
