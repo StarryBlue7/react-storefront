025097ce3862e37aa6483064af425d0e0b52d8e6
Vince <starryblue7@gmail.com>
2022-10-05 16:42:46 -0700

    add product page link to product cards

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,60 +1,74 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions, Chip, Box } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+  CardActions,
+  Chip,
+  Box,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function ProductCard({ product }: any) {
   return (
     <Card sx={{ height: "100%", display: "flex", flexFlow: "column" }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="280"
-          image={product.imgURL}
-          alt={product.fullName}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {product.shortName}
-          </Typography>
-          <Box
-            sx={{
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              display: "-webkit-box",
-              WebkitLineClamp: "1",
-              WebkitBoxOrient: "vertical",
-              mb: 1,
-            }}
-          >
-            {product.tags.map((tag: any, i: number) => (
-              <Chip
-                key={i}
-                label={tag.name}
-                variant="outlined"
-                size="small"
-                onClick={() => {}}
-                sx={{ mr: 0.5 }}
-              />
-            ))}
-          </Box>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              display: "-webkit-box",
-              WebkitLineClamp: "2",
-              WebkitBoxOrient: "vertical",
-            }}
-          >
-            {product.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link to={"/products/" + product._id} style={{ textDecoration: "none" }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="280"
+            image={product.imgURL}
+            alt={product.fullName}
+          />
+          <CardContent>
+            <Typography
+              color="primary"
+              gutterBottom
+              variant="h5"
+              component="div"
+            >
+              {product.shortName}
+            </Typography>
+            <Box
+              sx={{
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                display: "-webkit-box",
+                WebkitLineClamp: "1",
+                WebkitBoxOrient: "vertical",
+                mb: 1,
+              }}
+            >
+              {product.tags.map((tag: any, i: number) => (
+                <Chip
+                  key={i}
+                  label={tag.name}
+                  variant="outlined"
+                  size="small"
+                  onClick={() => {}}
+                  sx={{ mr: 0.5 }}
+                />
+              ))}
+            </Box>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                display: "-webkit-box",
+                WebkitLineClamp: "2",
+                WebkitBoxOrient: "vertical",
+              }}
+            >
+              {product.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
       <Box sx={{ flexGrow: 1 }} />
       <CardActions>
         <Button size="small" color="primary">
