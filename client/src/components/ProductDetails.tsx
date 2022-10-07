ffb5a69a0141e9775d06632f2b6c19ecdf69495b
Vince <starryblue7@gmail.com>
2022-10-07 16:12:24 -0700

    add model number & flexbox layout

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -21,12 +21,22 @@ export default function ProductDetails({ product }: any) {
             style={{ maxWidth: "100%", maxHeight: "50vh" }}
           />
         </Grid>
-        <Grid item xs={12} sm={5} sx={{}}>
+        <Grid
+          item
+          xs={12}
+          sm={5}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "left",
+          }}
+        >
           <Typography variant="h4">{product.fullName}</Typography>
-          <HeartRating value={product.rating || 4.5} readOnly />
+          <Typography variant="caption">{product.modelNumber}</Typography>
+          <HeartRating value={product.rating || 4.5} />
           <Typography variant="h4">{"$" + product.price}</Typography>
+          <Button variant="contained">Add to Cart</Button>
           <Typography sx={{ mt: 5 }}>{product.description}</Typography>
-          <Button>Add to Cart</Button>
         </Grid>
       </Grid>
     </Card>
