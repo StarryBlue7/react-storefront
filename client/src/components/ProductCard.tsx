0fd7780c868123270195605eb63e1376556a9e36
Vince <starryblue7@gmail.com>
2022-10-06 18:06:45 -0700

    add compact option for carousels

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -12,60 +12,81 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
-export default function ProductCard({ product }: any) {
+const styles = {
+  compactImg: {
+    height: 200,
+  },
+  img: {
+    height: 280,
+  },
+};
+
+export default function ProductCard({ product, compact }: any) {
   return (
-    <Card sx={{ height: "100%", display: "flex", flexFlow: "column" }}>
+    <Card
+      sx={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexFlow: "column",
+      }}
+    >
       <Link to={"/products/" + product._id} style={{ textDecoration: "none" }}>
         <CardActionArea>
           <CardMedia
             component="img"
-            height="280"
+            style={compact ? styles.compactImg : styles.img}
             image={product.imgURL}
             alt={product.fullName}
           />
           <CardContent>
             <Typography
               color="primary"
-              gutterBottom
+              gutterBottom={!compact}
+              align={"left"}
               variant="h5"
               component="div"
             >
               {product.shortName}
             </Typography>
-            <Box
-              sx={{
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                display: "-webkit-box",
-                WebkitLineClamp: "1",
-                WebkitBoxOrient: "vertical",
-                mb: 1,
-              }}
-            >
-              {product.tags.map((tag: any, i: number) => (
-                <Chip
-                  key={i}
-                  label={tag.name}
-                  variant="outlined"
-                  size="small"
-                  onClick={() => {}}
-                  sx={{ mr: 0.5 }}
-                />
-              ))}
-            </Box>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              sx={{
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                display: "-webkit-box",
-                WebkitLineClamp: "2",
-                WebkitBoxOrient: "vertical",
-              }}
-            >
-              {product.description}
-            </Typography>
+            {!compact && (
+              <>
+                <Box
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: "1",
+                    WebkitBoxOrient: "vertical",
+                    mb: 1,
+                  }}
+                >
+                  {product.tags.map((tag: any, i: number) => (
+                    <Chip
+                      key={i}
+                      label={tag.name}
+                      variant="outlined"
+                      size="small"
+                      onClick={() => {}}
+                      sx={{ mr: 0.5 }}
+                    />
+                  ))}
+                </Box>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: "2",
+                    WebkitBoxOrient: "vertical",
+                  }}
+                >
+                  {product.description}
+                </Typography>
+              </>
+            )}
           </CardContent>
         </CardActionArea>
       </Link>
