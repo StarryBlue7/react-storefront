1f025e99d87b25298b6ff17cb45a93a568f2ede0
Vince <starryblue7@gmail.com>
2022-10-10 16:55:29 -0700

    add tabbing between login/signup forms

--- client/src/components/AuthModal.tsx
+++ client/src/components/AuthModal.tsx
@@ -1,12 +1,27 @@
-import { Dialog } from "@mui/material";
+import React from "react";
+import { Dialog, Tabs, Tab } from "@mui/material";
 
 /**
  * Modal dialog for login/signup
  */
 export default function AuthModal({ modalStates }: any) {
+  const [authTab, setAuthTab] = React.useState<number>(0);
+  const changeTab = (_event: React.SyntheticEvent, tabIndex: number) => {
+    setAuthTab(tabIndex);
+  };
+
   return (
     <Dialog open={modalStates.authOpen} onClose={modalStates.closeAuth}>
-      Text text text
+      <Tabs
+        value={authTab}
+        onChange={changeTab}
+        indicatorColor="primary"
+        centered
+      >
+        <Tab label="Login" />
+        <Tab label="Sign Up" />
+      </Tabs>
+      {authTab ? <>Login</> : <>Signup</>}
     </Dialog>
   );
 }
