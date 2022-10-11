b004918874758804316313cfba01c3498f9ba37e
Vince <starryblue7@gmail.com>
2022-10-11 15:43:03 -0700

    add password length & confirmation match validation

--- client/src/utils/formValidations.ts
+++ client/src/utils/formValidations.ts
@@ -1,11 +1,12 @@
 const alphanumeric = new RegExp(/^\w+$/);
 
 type UsernameValidation = { usernameError: boolean; usernameHelper: string };
+type PasswordValidation = { passwordError: boolean; passwordHelper: string };
 
 class FormValidation {
   username(username: string): UsernameValidation {
-    let allowedChars = alphanumeric.test(username);
-    let length = username.length >= 3;
+    const allowedChars = alphanumeric.test(username);
+    const length = username.length >= 3;
 
     const usernameError = !(allowedChars && length);
 
@@ -17,6 +18,25 @@ class FormValidation {
     }
 
     return { usernameError, usernameHelper };
+  }
+
+  password(password: string, confirm: string): PasswordValidation {
+    let matchConfirm = true;
+    if (password.length > 0 && confirm.length > 0) {
+      matchConfirm = password === confirm;
+    }
+    const length = password.length >= 8;
+
+    const passwordError = !(matchConfirm && length);
+
+    let passwordHelper: string = " ";
+    if (!matchConfirm) {
+      passwordHelper = "Passwords must match.";
+    } else if (!length) {
+      passwordHelper = "Minimum 8 characters long.";
+    }
+
+    return { passwordError, passwordHelper };
   }
 }
 
