fa3a30477f775e552a6f6870fd1ea44300792d2b
Vince <starryblue7@gmail.com>
2022-10-11 18:15:58 -0700

    implement signup component

--- client/src/components/AuthModal.tsx
+++ client/src/components/AuthModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Dialog, Tabs, Tab, Container } from "@mui/material";
 
 import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 
 /**
  * Modal dialog for login/signup
@@ -24,7 +25,11 @@ export default function AuthModal({ modalStates }: any) {
         <Tab label="Sign Up" />
       </Tabs>
       <Container sx={{ pb: 1 }}>
-        {authTab === 0 ? <LoginForm modalStates={modalStates} /> : <>Signup</>}
+        {authTab === 0 ? (
+          <LoginForm modalStates={modalStates} />
+        ) : (
+          <SignupForm modalStates={modalStates} />
+        )}
       </Container>
     </Dialog>
   );
