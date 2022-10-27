6e1b2e1f2cc190136c9b898211d30e8323b00165
Vince <starryblue7@gmail.com>
2022-10-27 10:36:12 -0700

    add stripe webhook

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,14 @@
 const express = require("express");
 const path = require("path");
-const db = require("./config/connection");
+require("dotenv").config();
+
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
+
+const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
+
+const stripe = require("stripe");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +16,36 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
+});
+
+// Stripe webhook
+app.post("/webhook", express.raw({ type: "application/json" }), (req, res) => {
+  const sig = req.headers["stripe-signature"];
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.WEBHOOK_SECRET
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "payment_intent.succeeded":
+      const paymentIntent = event.data.object;
+      console.log("pay success: ", paymentIntent?.id);
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Acknowledge receipt of the event
+  res.send();
 });
 
 app.use(express.urlencoded({ extended: true }));
