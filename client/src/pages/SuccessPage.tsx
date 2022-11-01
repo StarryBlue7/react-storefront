08da5efb3e4f4cba892a46ac26297f86fa047b0c
Vince <starryblue7@gmail.com>
2022-11-01 10:43:16 -0700

    add item details to order display

--- client/src/pages/SuccessPage.tsx
+++ client/src/pages/SuccessPage.tsx
@@ -25,7 +25,6 @@ export default function SuccessPage({ cartHandler }: any) {
 
   const orderDetails = {
     label: "Order Details",
-    // headers: ["Field", "Value"],
     entries: [
       { row: "Order Number", values: [order.orderNum] },
       {
@@ -38,20 +37,41 @@ export default function SuccessPage({ cartHandler }: any) {
     ],
   };
 
+  const orderItems = {
+    label: "Order Items",
+    headers: ["Item", "Model", "Qty.", "Price", "Total"],
+    entries: order.items.map((item: any) => {
+      console.log(item);
+      return {
+        row: item.product.fullName,
+        values: [
+          item.product.modelNumber,
+          item.quantity,
+          item.product.price,
+          (item.quantity * item.product.price).toFixed(2),
+        ],
+      };
+    }),
+    footers: [{ row: "Subtotal" }],
+  };
+
   return loading || !data ? (
     <Typography variant="h3">Loading...</Typography>
   ) : (
     <>
       <Grid container flexDirection="column" width="100%" rowGap={2}>
+        <Grid item md={12} flexGrow={1} pt={2} px={2}>
+          <Typography variant="h5" sx={{ pt: 2, px: 2 }}>
+            Order Complete!
+          </Typography>
+        </Grid>
         <Grid container flexDirection="row">
           <Grid item md={6} flexGrow={1} p={2}>
-            <Typography variant="h5" sx={{ p: 2 }}>
-              Order Complete!
-            </Typography>
             <DetailsTable data={orderDetails} />
           </Grid>
           <Grid item md={6} flexGrow={1} p={2}>
-            <Cart cartHandler={cartHandler} label="Items" disable />
+            {/* <Cart cartHandler={cartHandler} label="Items" disable /> */}
+            <DetailsTable data={orderItems} />
           </Grid>
         </Grid>
         <Divider />
