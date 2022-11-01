18bfae2190c94e8d3939b3f65a6881a30896fa5e
Vince <starryblue7@gmail.com>
2022-10-31 17:37:53 -0700

    add order details via details table component

--- client/src/pages/SuccessPage.tsx
+++ client/src/pages/SuccessPage.tsx
@@ -1,25 +1,17 @@
 import React from "react";
+import { Divider, Grid, Typography } from "@mui/material";
+
 import { useSearchParams } from "react-router-dom";
-import {
-  Divider,
-  Grid,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-} from "@mui/material";
 import { useQuery } from "@apollo/client";
 import { QUERY_ORDER } from "../utils/queries";
-import Cart from "../components/Cart";
 
-// http://localhost:3000/success?payment_intent=pi_3Lz0qZG2F40Ds5140l3exg1V&payment_intent_client_secret=pi_3Lz0qZG2F40Ds5140l3exg1V_secret_9ksyGA29WjoVSWPKkFrvFLYsp&redirect_status=succeeded
+import Cart from "../components/Cart";
+import DetailsTable from "../components/DetailsTable";
 
 /**
  * Individual product page
  */
 export default function SuccessPage({ cartHandler }: any) {
-  // const { productId } = useParams();
-
   const [searchParams] = useSearchParams();
 
   const stripeId = searchParams.get("payment_intent");
@@ -31,9 +23,20 @@ export default function SuccessPage({ cartHandler }: any) {
 
   const order = data?.order || {};
 
-  // React.useEffect(() => {
-  //   console.log(data);
-  // }, [data, loading]);
+  const orderDetails = {
+    label: "Order Details",
+    // headers: ["Field", "Value"],
+    entries: [
+      { row: "Order Number", values: [order.orderNum] },
+      {
+        row: "Order Placed",
+        values: [new Date(parseInt(order.paidOn)).toLocaleString()],
+      },
+      { row: "Subtotal", values: ["$" + order.subtotal] },
+      { row: "Shipping Address", values: [order.toAddress] },
+      { row: "Estimated Arrival", values: [order.estimatedArrival || "TBD"] },
+    ],
+  };
 
   return loading || !data ? (
     <Typography variant="h3">Loading...</Typography>
@@ -45,11 +48,7 @@ export default function SuccessPage({ cartHandler }: any) {
             <Typography variant="h5" sx={{ p: 2 }}>
               Order Complete!
             </Typography>
-            <List>
-              <ListItem>
-                <ListItemText>Order Number: {order.orderNum}</ListItemText>
-              </ListItem>
-            </List>
+            <DetailsTable data={orderDetails} />
           </Grid>
           <Grid item md={6} flexGrow={1} p={2}>
             <Cart cartHandler={cartHandler} label="Items" disable />
