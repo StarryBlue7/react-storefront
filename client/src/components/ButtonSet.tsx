820981f79441b0ac5e4b31ec8d4742d7045e638c
Vince <starryblue7@gmail.com>
2022-11-02 13:23:32 -0700

    add alignment & 'none' icon options

--- client/src/components/ButtonSet.tsx
+++ client/src/components/ButtonSet.tsx
@@ -10,12 +10,16 @@ import { StarBorder } from "@mui/icons-material";
 
 import { Link } from "react-router-dom";
 
+type Align = "left" | "center" | "right";
+
 type ButtonSetProps = {
   buttons: ButtonConfig[];
+  span?: boolean;
+  align?: Align;
 };
 
 type ButtonConfig = {
-  icon?: React.ReactElement;
+  icon?: React.ReactElement | "none";
   label: string;
   path?: string;
   disabled?: boolean;
@@ -30,28 +34,52 @@ const styles = {
   cartButtonIcon: { minWidth: 0, mr: 1 },
 };
 
-export default function ButtonSet({ buttons }: ButtonSetProps) {
+export default function ButtonSet({
+  buttons,
+  span = false,
+  align,
+}: ButtonSetProps) {
+  let justification = "center";
+  switch (align) {
+    case "left":
+      justification = "flex-start";
+      break;
+    case "right":
+      justification = "flex-end";
+  }
   return (
     <>
       <List>
         <ListItem
-          key={"cart-options"}
-          sx={{ display: "flex", flexFlow: "row wrap", gap: 1 }}
+          sx={{
+            display: "flex",
+            flexFlow: "row wrap",
+            gap: 1,
+            justifyContent: justification,
+          }}
         >
           {buttons.map((button: any) => (
             <Link
               to={!button.disabled && button.path}
               key={button.label}
-              style={styles.links}
+              style={{ ...styles.links, flexGrow: span ? 1 : 0 }}
             >
               <Button
                 onClick={button.function}
                 disabled={button.disabled}
-                variant={button.variant}
+                variant={button.variant || "contained"}
                 sx={styles.cartButton}
               >
                 <ListItemIcon sx={styles.cartButtonIcon}>
-                  {button.icon || <StarBorder />}
+                  {button.icon || (
+                    <StarBorder
+                      sx={{
+                        color: !(button.variant === "contained")
+                          ? "white"
+                          : "gray",
+                      }}
+                    />
+                  )}
                 </ListItemIcon>
                 <ListItemText primary={button.label} />
               </Button>
