720dafbf1ea95099330f5b1b09601e9a069df709
Vince <starryblue7@gmail.com>
2022-11-02 16:44:17 -0700

    prevent icon flex grow for non-mobile views

--- client/src/components/cart/CartItem.tsx
+++ client/src/components/cart/CartItem.tsx
@@ -122,7 +122,7 @@ export default function CartItem({
               color: "black",
             }}
           >
-            <ListItemIcon sx={{ flexGrow: options ? 1 : 0 }}>
+            <ListItemIcon sx={{ flexGrow: { xs: options ? 1 : 0, sm: 0 } }}>
               <img
                 src={item.product.imgURL}
                 alt={item.product.shortName}
