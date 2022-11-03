39fea938c3dc8e1ad9dbdfa851dabcb6ce7e4744
Vince <starryblue7@gmail.com>
2022-11-03 16:15:12 -0700

    pass category states to breadcrumbs props

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CategoryBreadcrumbs from "../components/categories/CategoryBreadcrumbs";
 import ProductsResults from "../components/product/ProductsResults";
 import PromoCarousel from "../components/PromoCarousel";
 import TagList from "../components/TagList";
@@ -11,6 +12,7 @@ export default function Home({ tagStates, categoryStates, cartHandler }: any) {
     <>
       <PromoCarousel />
       <TagList tagStates={tagStates} />
+      <CategoryBreadcrumbs categoryStates={categoryStates} />
       <ProductsResults
         tagStates={tagStates}
         categoryStates={categoryStates}
