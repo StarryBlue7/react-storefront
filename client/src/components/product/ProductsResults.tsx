ba175dad4834fa3be1667e52c7d3f59f5e4e8fc1
Vince <starryblue7@gmail.com>
2022-11-10 12:33:59 -0800

    add pagination component functionality

--- client/src/components/product/ProductsResults.tsx
+++ client/src/components/product/ProductsResults.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { ChangeEvent, useMemo, useState } from "react";
 
-import { Grid } from "@mui/material";
+import { Grid, Pagination } from "@mui/material";
 
 import { useQuery } from "@apollo/client";
 import { QUERY_PRODUCTS } from "../../utils/queries";
@@ -55,41 +55,76 @@ type ProductResultsProps = {
   categoryId?: string;
   tagStates: TagStates;
   cartHandler: CartHandler;
+  page?: number;
+  perPage?: number;
 };
 
 export default function ProductsResults({
   categoryId,
   tagStates,
   cartHandler,
+  page = 1,
+  perPage = 12,
 }: ProductResultsProps) {
+  // Results page state
+  const [currentPage, setCurrentPage] = useState<number>(page);
+
+  const changePage = (_e: ChangeEvent<unknown>, value: number) => {
+    setCurrentPage(value);
+  };
+
+  // Query products with tag/category filters
   const { loading, data } = useQuery(QUERY_PRODUCTS, {
     variables: {
       tags: tagStates?.selectedTags?.size
         ? Array.from(tagStates.selectedTags)
         : null,
       category: categoryId || null,
-      page: 1,
-      perPage: 12,
+      page: currentPage,
+      perPage,
     },
     fetchPolicy: "no-cache",
   });
 
-  const products = data?.products || [];
+  const products = data?.products.results || [];
+  const productCount = data?.products.pagination.count;
+
+  const totalPages = useMemo(() => {
+    return Math.ceil(productCount / perPage);
+  }, [productCount, perPage]);
 
   return (
     <Grid container spacing={2}>
       {loading ? (
         <Loader message="Loading results..." />
       ) : (
-        products.map((product: Product, i: number) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} m={0} key={i}>
-            <ProductCard
-              product={product}
-              key={product._id}
-              cartHandler={cartHandler}
+        <>
+          <Grid container justifyContent="center" mt={2}>
+            <Pagination
+              count={totalPages}
+              page={currentPage}
+              onChange={changePage}
+              color="primary"
             />
           </Grid>
-        ))
+          {products.map((product: Product, i: number) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} m={0} key={i}>
+              <ProductCard
+                product={product}
+                key={product._id}
+                cartHandler={cartHandler}
+              />
+            </Grid>
+          ))}
+          <Grid container justifyContent="center" mt={2}>
+            <Pagination
+              count={totalPages}
+              page={currentPage}
+              onChange={changePage}
+              color="primary"
+            />
+          </Grid>
+        </>
       )}
     </Grid>
   );
